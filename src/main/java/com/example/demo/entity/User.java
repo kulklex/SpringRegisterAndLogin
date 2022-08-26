bdfd736b0898579ee4bb0b23aaa119d4f6b77944
kulklex <kulklex@gmail.com>
2022-08-26 19:57:17 +0100

    Setting up first Service,Controller, Entity and Repository

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,26 @@
 package com.example.demo.entity;
 
 
-import javax.persistence.Entity;
+import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
+@Data
 public class User {
-    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(length = 60)
+    private String password;
+
+    private String role;
+    private boolean enabled = false;
+
+
 }
